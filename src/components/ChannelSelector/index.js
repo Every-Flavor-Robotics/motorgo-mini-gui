0dc85db38fe93cdff362166710cc1e6b05fd12f0
Swapnil Pande <swapnil.pande98@gmail.com>
2023-11-13 14:13:14 -0500

    Basic PID parameter loading and saving working

--- src/components/ChannelSelector/index.js
+++ src/components/ChannelSelector/index.js
@@ -1,10 +1,80 @@
-import React, { useState } from 'react';
-import { Box, Tabs, TabList, Tab, TabPanels, TabPanel, useColorModeValue } from '@chakra-ui/react';
+import React, { useState, useRef } from 'react';
+import {
+    Box, Tabs, TabList, Tab, TabPanels, TabPanel,
+    Button, HStack, useColorModeValue, Spinner, useToast
+} from '@chakra-ui/react';
 import TuningOptions from '../TuningOptions';
 
 const ChannelSelector = () => {
     const [selectedChannel, setSelectedChannel] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isUpdating, setIsUpdating] = useState(false);
     const tabBgColor = useColorModeValue("gray.100", "gray.700");
+    const toast = useToast(); // Initialize the toast hook
+
+    // Create refs for each TuningOptions instance
+    const ch0PositionRef = useRef();
+    const ch0VelocityRef = useRef();
+    const ch0TorqueRef = useRef();
+
+    const ch1PositionRef = useRef();
+    const ch1VelocityRef = useRef();
+    const ch1TorqueRef = useRef();
+
+    const showToast = (title, description, status) => {
+        toast({
+            title: title,
+            description: description,
+            status: status,
+            duration: 5000,
+            isClosable: true,
+            position: 'bottom-left',
+        });
+    };
+
+    // Function to handle loading state and call the provided operation
+    const handleOperation = async (operation, operationStateSetter, operationName) => {
+        operationStateSetter(true);
+        try {
+            await operation();
+            // Sleep for 1 second to allow the controller to update
+            await new Promise(r => setTimeout(r, 500));
+            showToast(`${operationName} Successful`, ``, 'success');
+        } catch (error) {
+            await new Promise(r => setTimeout(r, 500));
+            console.error(`${operationName} failed:`, error);
+            showToast(`${operationName} Failed`, ``, 'error');
+        }
+        operationStateSetter(false);
+    };
+
+    const saveAll = () => {
+        handleOperation(() => {
+            // Call save method on each TuningOptions
+            ch0PositionRef.current?.saveData();
+            ch0VelocityRef.current?.saveData();
+            ch0TorqueRef.current?.saveData();
+
+            ch1PositionRef.current?.saveData();
+            ch1VelocityRef.current?.saveData();
+            ch1TorqueRef.current?.saveData();
+
+        }, setIsUpdating, "Push Parameters");
+    };
+
+    const loadAll = () => {
+        handleOperation(() => {
+            // Call load method on each TuningOptions
+            ch0PositionRef.current?.loadData();
+            ch0VelocityRef.current?.loadData();
+            ch0TorqueRef.current?.loadData();
+
+            ch1PositionRef.current?.loadData();
+            ch1VelocityRef.current?.loadData();
+            ch1TorqueRef.current?.loadData();
+
+        }, setIsLoading, "Load Parameters");
+    };
 
     const handleTabChange = (index) => {
         setSelectedChannel(index);
@@ -19,17 +89,37 @@ const ChannelSelector = () => {
                 </TabList>
                 <TabPanels>
                     <TabPanel px={4} pt={4}>
-                        <TuningOptions title="Position Controller" mb={4} />
-                        <TuningOptions title="Velocity Controller" mb={4} />
-                        <TuningOptions title="Torque Controller" />
+                        <TuningOptions ref={ch0PositionRef} title="Position Controller" channel={0} controller_type={"position"} mb={4} />
+                        <TuningOptions ref={ch0VelocityRef} title="Velocity Controller" channel={0} controller_type={"velocity"} mb={4} />
+                        <TuningOptions ref={ch0TorqueRef} title="Torque Controller" channel={0} controller_type={"current"} />
                     </TabPanel>
                     <TabPanel px={4} pt={4}>
-                        <TuningOptions title="Position Controller" mb={4} />
-                        <TuningOptions title="Velocity Controller" mb={4} />
-                        <TuningOptions title="Torque Controller" />
+                        <TuningOptions ref={ch1PositionRef} title="Position Controller" channel={1} controller_type={"position"} mb={4} />
+                        <TuningOptions ref={ch1VelocityRef} title="Velocity Controller" channel={1} controller_type={"velocity"} mb={4} />
+                        <TuningOptions ref={ch1TorqueRef} title="Torque Controller" channel={1} controller_type={"current"} />
                     </TabPanel>
                 </TabPanels>
             </Tabs>
+            <HStack spacing={4} mt={4} justifyContent="center">
+                <Button
+                    leftIcon={isLoading ? <Spinner size="sm" /> : null}
+                    colorScheme="blue"
+                    onClick={loadAll}
+                    isLoading={isLoading}
+                    loadingText="Loading..."
+                >
+                    Load Parameters
+                </Button>
+                <Button
+                    leftIcon={isUpdating ? <Spinner size="sm" /> : null}
+                    colorScheme="green"
+                    onClick={saveAll}
+                    isLoading={isUpdating}
+                    loadingText="Updating..."
+                >
+                    Update Parameters
+                </Button>
+            </HStack>
         </Box>
     );
 };
--- src/components/TuningOptions/index.js
+++ src/components/TuningOptions/index.js
@@ -1,29 +1,130 @@
-import React, { useState } from 'react';
+import React, { forwardRef, useImperativeHandle, useState, useEffect } from 'react';
 import { Input, VStack, HStack, Text, useColorModeValue } from "@chakra-ui/react";
 
-const TuningOptions = ({ title, mb }) => {
+const TuningOptions = forwardRef(({ title, channel, mb, controller_type }, ref) => {
     const [pGain, setPGain] = useState('');
     const [iGain, setIGain] = useState('');
     const [dGain, setDGain] = useState('');
     const [lpf, setLPF] = useState('');
 
+    // State to keep track of the input values before formatting
+    const [pGainInput, setPGainInput] = useState('');
+    const [iGainInput, setIGainInput] = useState('');
+    const [dGainInput, setDGainInput] = useState('');
+    const [lpfInput, setLPFInput] = useState('');
+
     const bgColor = useColorModeValue("gray.100", "gray.700");
     const borderColor = useColorModeValue("gray.200", "gray.600");
+
+    const formatValue = (value) => {
+        // Check if value is a valid number (including zero)
+        if (!isNaN(value) && value !== '') {
+            return parseFloat(parseFloat(value).toFixed(4)).toString(); // Convert to a float and back to a string to remove trailing zeros
+        }
+    };
+
+    // Call this function when the input is blurred
+    const handleBlur = (setter, inputValue, setInputChange) => {
+        const formattedValue = formatValue(inputValue);
+        setter(formattedValue); // Set the state with the formatted value
+        setInputChange(formattedValue); // Also update the input state to display the formatted value
+    };
+
+    useEffect(() => {
+        loadData();
+    }, [channel, controller_type]); // Dependency array, so it only runs once when the component mounts or if channel changes
+
+    // Expose the save and load methods to the parent component
+    useImperativeHandle(ref, () => ({
+        saveData,
+        loadData
+    }));
+
+    const handleInputChange = (setter, value) => {
+        const number = parseFloat(value); // Convert the input value to a float
+        if (!isNaN(number)) { // Check if the conversion was successful
+            setter(number); // If successful, update the state with the float number
+        } else {
+            setter(''); // If conversion failed (e.g., empty string), reset the state
+        }
+    };
+
+    const saveData = async () => {
+        // Set the loading state here if you have one
+        try {
+            // Parse the input state to a float before sending
+            const pGainNumber = parseFloat(pGainInput);
+            const iGainNumber = parseFloat(iGainInput);
+            const dGainNumber = parseFloat(dGainInput);
+            const lpfNumber = parseFloat(lpfInput);
+
+            // Make sure to check if the parsed number is a valid number before sending
+            if (!isNaN(pGainNumber)) {
+                await updateGain(channel, controller_type, 'p', pGainNumber);
+            }
+            if (!isNaN(iGainNumber)) {
+                await updateGain(channel, controller_type, 'i', iGainNumber);
+            }
+            if (!isNaN(dGainNumber)) {
+                await updateGain(channel, controller_type, 'd', dGainNumber);
+            }
+            if (!isNaN(lpfNumber)) {
+                await updateGain(channel, controller_type, 'lpf', lpfNumber);
+            }
+        } catch (error) {
+            console.error('Failed to save PID parameters:', error);
+        }
+        // Unset the loading state here
+    };
+
+    const loadData = async () => {
+        try {
+            const responseP = await fetch(`http://192.168.0.132:8080/ch${channel}/${controller_type}/p`);
+            const dataP = await responseP.json();
+            setPGain(formatValue(dataP.value));
+            setPGainInput(formatValue(dataP.value)); // Update the input state as well
+
+            const responseI = await fetch(`http://192.168.0.132:8080/ch${channel}/${controller_type}/i`);
+            const dataI = await responseI.json();
+            setIGain(formatValue(dataI.value));
+            setIGainInput(formatValue(dataI.value)); // Update the input state as well
+
+            const responseD = await fetch(`http://192.168.0.132:8080/ch${channel}/${controller_type}/d`);
+            const dataD = await responseD.json();
+            setDGain(formatValue(dataD.value));
+            setDGainInput(formatValue(dataD.value)); // Update the input state as well
+
+            const responseLPF = await fetch(`http://192.168.0.132:8080/ch${channel}/${controller_type}/lpf`);
+            const dataLPF = await responseLPF.json();
+            setLPF(formatValue(dataLPF.value));
+            setLPFInput(formatValue(dataLPF.value)); // Update the input state as well
+        } catch (error) {
+            console.error('Failed to load PID parameters:', error);
+        }
+    };
 
     return (
         <VStack spacing={4} align="start" bg={bgColor} p={5} borderRadius="md" border="1px" borderColor={borderColor} mb={mb} w="100%" boxShadow="md">
             <Text fontSize="xl" fontWeight="bold" color="orange.800">{title}</Text>
             <HStack spacing={4} w="100%" justify="space-between">
-                {[{ label: 'P Gain', value: pGain, setter: setPGain },
-                { label: 'I Gain', value: iGain, setter: setIGain },
-                { label: 'D Gain', value: dGain, setter: setDGain },
-                { label: 'LPF Time Constant', value: lpf, setter: setLPF }].map((item, index) => (
+                {[{
+                    label: 'P Gain', value: pGain, setter: setPGain, inputState: pGainInput, setInputState: setPGainInput
+                }, {
+                    label: 'I Gain', value: iGain, setter: setIGain, inputState: iGainInput, setInputState: setIGainInput
+                }, {
+                    label: 'D Gain', value: dGain, setter: setDGain, inputState: dGainInput, setInputState: setDGainInput
+                }, {
+                    label: 'LPF Time Constant', value: lpf, setter: setLPF, inputState: lpfInput, setInputState: setLPFInput
+                }].map((item, index) => (
                     <VStack key={index}>
                         <Text>{item.label}:</Text>
                         <Input
+                            type="number"
+                            step="any"
                             variant="outline"
-                            value={item.value}
-                            onChange={(e) => item.setter(e.target.value)}
+                            value={item.inputState}
+                            onChange={(e) => item.setInputState(e.target.value)}
+                            onBlur={() => handleBlur(item.setter, item.inputState, item.setInputState)}
                             placeholder={`Enter ${item.label}`}
                             size="sm"
                             focusBorderColor="orange.800"
@@ -33,6 +134,25 @@ const TuningOptions = ({ title, mb }) => {
             </HStack>
         </VStack>
     );
-}
+});
 
 export default TuningOptions;
+
+// Helper function to update a gain value
+const updateGain = async (channel, controller_type, gain_type, value) => {
+    try {
+        const response = await fetch(`http://192.168.0.132:8080/ch${channel}/${controller_type}/${gain_type}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ value })
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    } catch (error) {
+        console.error(`Failed to update ${gain_type} gain:`, error);
+    }
+};
